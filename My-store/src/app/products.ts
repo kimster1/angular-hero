ff749e4abf77d39e5198dc840effa17784a63e28
kimster1 <1205719281@qq.com>
2019-08-17 08:39:59 -0700

    the first commit of My-store

---
+++ My-store/src/app/products.ts
@@ -0,0 +1,27 @@
+
+export  const products=[
+      {   name:'iPhoneX',
+          price: 5000,
+          description: 'This is the tenth generation iPhone'             
+    },
+    
+    {   name:'iPhoneXs',
+        price:8199,
+        description: 'Upgrated version of iPhoneX'              
+    },
+
+    {
+         name:'iPhoneXs Max',
+         price:9099,
+         description:  'Large screen '
+    },
+
+    {
+        name:'iPhoneXR',
+        price:6199,
+        //description:  'It is a brillant upgrated'
+    }
+ 
+
+
+]
