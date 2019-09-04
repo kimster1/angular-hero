5fadaa0ff884d6b78d94058c24e7ef3bfc5f281e
kimster1 <1205719281@qq.com>
2019-09-04 06:48:19 -0700

    DoCheck

--- OnChanges-hook/src/app/plants/plants.component.ts
+++ OnChanges-hook/src/app/plants/plants.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit,  Input, OnChanges} from '@angular/core';
+import { Component, OnInit,  Input, OnChanges, DoCheck} from '@angular/core';
 
 @Component({
   selector: 'app-plants',
   templateUrl: './plants.component.html',
   styleUrls: ['./plants.component.css']
 })
-export class PlantsComponent implements OnInit,OnChanges{
+export class PlantsComponent implements OnInit,OnChanges,DoCheck{
+ 
  
   
    @Input()
@@ -16,12 +17,31 @@ export class PlantsComponent implements OnInit,OnChanges{
 
     message:string = "初始化消息";
 
+    oldUsername:string;
+    changeDetected:boolean=false;
+    noChangeCount:number =0;
+
   constructor() { }
 
   ngOnInit() {
    
   }
  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
-    console.log(JSON.stringify(changes,null,2));
+    console.log("hello"+JSON.stringify(changes,null,2));
+  } 
+  
+  ngDoCheck(): void {
+      if(this.user.name!==this.oldUsername){
+        this.changeDetected=true;
+        console.log("DoCheck:user.name从"+this.oldUsername+"变为"+this.user.name);
+        this.oldUsername=this.user.name;
+      }
+         if(this.changeDetected){
+           this.noChangeCount=0;
+         } else{
+           this.noChangeCount=this.noChangeCount+1;
+           console.log("DoCheck:user.name没变化时ngDoCheck方法已经被调用"+this.noChangeCount)
+         }
+         this.changeDetected=false;
   }
 }
