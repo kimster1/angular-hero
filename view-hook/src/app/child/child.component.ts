4b2d0713ff525a8647b7e4c6d050acec68cca3f8
kimster1 <1205719281@qq.com>
2019-09-04 07:40:52 -0700

    view-hook

---
+++ view-hook/src/app/child/child.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+ 
+  greeting(name:string){
+    console.log("hello"+name);
+  }
+
+
+}
