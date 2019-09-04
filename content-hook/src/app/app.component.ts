81f3a2d3374bdc9f71dee8e9e4668dcd229fed59
kimster1 <1205719281@qq.com>
2019-09-04 08:39:37 -0700

    content-hook

---
+++ content-hook/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'content-hook';
+
+  divContent="<div>mu ke wang</div>";
+}
