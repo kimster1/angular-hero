2e6389f33d56f53ed4e4024079deb138155aafa3
kimster1 <1205719281@qq.com>
2019-09-27 08:27:54 -0700

    a little problem

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import{Hero} from '../hero';
-import {HeroService} from '../hero.service';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,18 +9,38 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
+  constructor(private heroService: HeroService) { }
 
-  heroes:Hero[];
- 
-
-  constructor(private heroService:HeroService) { }
-   
   ngOnInit() {
-     this.getHeroes();
+    this.getHeroes();
   }
- getHeroes():void{
-   this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes=heroes);
-     }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- angular-tour-of-heroes/src/app/message.service.ts
+++ angular-tour-of-heroes/src/app/message.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 
-
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class MessageService {
-   messages:string[]=[];
-  add(message:string){
-    this.messages.push(message);
+  messages: string[] = [];
 
+  add(message: string) {
+    this.messages.push(message);
   }
-  clear(){
-    this.messages=[];
+
+  clear() {
+    this.messages = [];
   }
-  constructor() { }
 }
